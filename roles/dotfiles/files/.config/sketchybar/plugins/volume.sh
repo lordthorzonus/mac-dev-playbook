3995c39579363f923601f9852bee5cfc0f818350
Juuso Leinonen <leinonen.juuso@gmail.com>
2023-09-08 12:28:55 +0300

    Random changes from muscle memory that I messed up when cleaning the config

--- roles/dotfiles/files/.config/sketchybar/plugins/volume.sh
+++ roles/dotfiles/files/.config/sketchybar/plugins/volume.sh
@@ -3,8 +3,7 @@
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
-if [ "$SENDER" = "volume_change" ]; then
-    VOLUME=$INFO
+if [ "$SENDER" = "volume_change" ]; then VOLUME=$INFO
 
     case $VOLUME in
         [6-9][0-9] | 100)
