7dac1c74a65b471c3603919afeb3a6ac33650ed4
Juuso Leinonen <juuso.leinonen@smartly.io>
2024-11-29 23:16:56 +0200

    Fix some configs

--- roles/dotfiles/files/.config/nvim/lua/plugins/indent-blankline.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,16 +1,23 @@
 return {
-	{
-		{
-			"lukas-reineke/indent-blankline.nvim",
-			main = "ibl",
-			--- @type ibl.config
-			opts = {
-				scope = {
-					enabled = true,
-					show_start = false,
-					show_exact_scope = true,
-				},
-			},
-		},
-	},
+    {
+        {
+            "lukas-reineke/indent-blankline.nvim",
+            main = "ibl",
+            --- @type ibl.config
+            opts = {
+                scope = {
+                    enabled = true,
+                    show_start = true,
+                    show_exact_scope = true,
+                },
+            },
+        },
+    },
+    -- {
+    --     'echasnovski/mini.indentscope',
+    --     version = '*',
+    --     opts = {
+    --         symbol = '|'
+    --     }
+    -- },
 }
--- roles/dotfiles/files/.config/nvim/lua/plugins/null-ls.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins/null-ls.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		version = false,
-		event = "BufRead",
-		dependencies = {
-			"ThePrimeagen/refactoring.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    {
+        "nvimtools/none-ls.nvim",
+        version = false,
+        event = "BufRead",
+        dependencies = {
+            "ThePrimeagen/refactoring.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.ktlint,
-					null_ls.builtins.formatting.rustywind,
-					null_ls.builtins.formatting.ocamlformat,
-					null_ls.builtins.formatting.prettierd,
-					null_ls.builtins.formatting.goimports_reviser,
-					null_ls.builtins.formatting.golines,
-					null_ls.builtins.code_actions.refactoring,
-					null_ls.builtins.code_actions.impl,
-					null_ls.builtins.code_actions.gomodifytags,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({
-							group = augroup,
-							buffer = bufnr,
-						})
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr, async = false })
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.ktlint,
+                    null_ls.builtins.formatting.rustywind,
+                    null_ls.builtins.formatting.ocamlformat,
+                    null_ls.builtins.formatting.prettierd,
+                    null_ls.builtins.formatting.goimports_reviser,
+                    null_ls.builtins.formatting.golines,
+                    null_ls.builtins.code_actions.refactoring,
+                    null_ls.builtins.code_actions.impl,
+                    null_ls.builtins.code_actions.gomodifytags,
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({
+                            group = augroup,
+                            buffer = bufnr,
+                        })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = bufnr, async = false })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
 }
